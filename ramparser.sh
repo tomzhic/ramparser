8aca3cbf2ff3669bd37a8cdf05f2684cc7bd7eb8
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2015-10-26 04:33:23 +0000

    add TZ parser
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- ramparser.sh
+++ ramparser.sh
@@ -13,6 +13,7 @@ echo "msm8992 ram parser AP start"
 rm -rf ap-log
 mkdir ap-log
 
+#ap
 python $base_path/./ramparse.py \
 	--nm-path $ndk_path/toolchains/aarch64-linux-android-4.9/prebuilt/windows-x86_64/bin/aarch64-linux-android-nm \
 	--gdb-path $ndk_path/toolchains/aarch64-linux-android-4.9/prebuilt/windows-x86_64/bin/aarch64-linux-android-gdb \
@@ -20,16 +21,7 @@ python $base_path/./ramparse.py \
 	-a . \
 	--outdir ./ap-log \
 	-x --64-bit --force-hardware 8992
-echo "msm8992 ram parser AP done" 
 
-#echo "msm8992 ram parser TZ start" 
-#python $base_path/tz_diag_parser.py OCIMEM.BIN DDRCS0_0.BIN > ./ap-logs/tz_log.txt
-#echo "msm8992 ram parser TZ done" 
-#
-#if [ -e RPM_AAAAANAAR.elf ]
-#then
-#    rm -rf rpm-log
-#    mkdir rpm-log
-# 
-#    python "$oem_root_path"/rpm_proc/core/bsp/rpm/scripts/hansei/hansei.py --elf ./RPM_AAAAANAAR.elf -o ./rpm-logs/ ./CODERAM.BIN ./DATARAM.BIN ./MSGRAM.BIN
-#fi
+#tz
+python $base_path/vendor/msm8992/tz_diag_parser.py OCIMEM.BIN DDRCS0_0.BIN > ./ap-log/tz_log.txt
+echo "msm8992 ram parser AP done"
